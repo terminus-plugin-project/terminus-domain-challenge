61ad749af11328764efdc2baa08760b16a3b3540
Sean Dietrich <sean@kanopi.com>
2024-05-24 16:38:27 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# Terminus Domain Challenge
+
+[![CircleCI](https://circleci.com/gh/terminus-plugin-project/terminus-domain-challenge.svg?style=shield)](https://circleci.com/gh/terminus-plugin-project/terminus-domain-challenge)
+[![Actively Maintained](https://img.shields.io/badge/Pantheon-Actively_Maintained-yellow?logo=pantheon&color=FFDC28)](https://pantheon.io/docs/oss-support-levels#actively-maintained-support)
+
+[![Terminus v3.x Compatible](https://img.shields.io/badge/terminus-2.x%20--%203.x-green.svg)](https://github.com/terminus-plugin-project/tree/1.x)
+
+A simple plugin for Terminus-CLI to get the Domain DNS challenge based on a site.
+
+Adds command 'domain:dns:challenge' to Terminus. Learn more about Terminus Plugins in the
+[Terminus Plugins documentation](https://pantheon.io/docs/terminus/plugins)
+
+## Configuration
+
+These commands require no configuration
+
+## Usage
+
+* `terminus domain:dns:challenge <site>.<env>`
+* `terminus domain:dns:challenge <site>.<env> --filter="domain=www.example.com"`
+
+## Installation
+
+To install this plugin using Terminus 3:
+
+```sh
+terminus self:plugin:install terminus-domain-challenge
+```
+
+## Testing
+
+This project includes four testing targets:
+
+* `composer lint`: Syntax-check all php source files.
+* `composer cs`: Code-style check.
+* `composer unit`: Run unit tests with phpunit
+* `composer functional`: Run functional test with bats
+
+To run all tests together, use `composer test`.
+
+Note that prior to running the tests, you should first run:
+
+* `composer install`
+* `composer install-tools`
+
+## Help
+
+Run `terminus help domain:dns:challenge` for help.
---
+++ src/Commands/DomainChallengesCommand.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Pantheon\TerminusDomainChallenge\Commands;
+
+use Consolidation\OutputFormatters\StructuredData\RowsOfFields;
+use Pantheon\Terminus\Commands\TerminusCommand;
+use Pantheon\Terminus\Site\SiteAwareInterface;
+use Pantheon\Terminus\Site\SiteAwareTrait;
+
+/**
+ * Get Information Related to the Domains Added.
+ */
+class DomainChallengesCommand extends TerminusCommand implements SiteAwareInterface
+{
+    use SiteAwareTrait;
+
+    /**
+     * Look up Challenge Records for the Provided Domains.
+     *
+     * @authorize
+     * @filter-output
+     *
+     * @command domain:dns:challenge
+     *
+     * @field-labels
+     *     domain: Domain
+     *     http_status: HTTP Status
+     *     http_key: HTTP Name
+     *     http_token: HTTP Token
+     *     dns_status: DNS Status
+     *     dns_key: DNS Key
+     *     dns_token: DNS Token
+     * @param string $site_env Site & environment in the format `site-name.env`
+     *
+     * @usage <site>.<env> Displays recommended DNS Challenges for <site>'s <env> environment.
+     *
+     * @return \Consolidation\OutputFormatters\StructuredData\RowsOfFields
+     *
+     * @throws \Pantheon\Terminus\Exceptions\TerminusException
+     */
+    public function getDomainChallenges($site_env)
+    {
+        $env = $this->getEnv($site_env);
+        $domains = $env->getDomains()->filter(
+            function ($domain) {
+                return $domain->get('type') === 'custom';
+            }
+        )->all();
+
+        $data = [];
+        foreach ($domains AS $domain) {
+            $acmeValues = (array)$domain->get('acme_preauthorization_challenges');
+            array_walk($acmeValues, function(&$item){ $item = (array)$item; });
+
+            $data[] = [
+                'domain' => $domain->id,
+                'http_status' => $acmeValues['http-01']['status'],
+                'http_key' => $acmeValues['http-01']['verification_key'],
+                'http_token' => $acmeValues['http-01']['token'],
+                'dns_status' => $acmeValues['dns-01']['status'],
+                'dns_key' => $acmeValues['dns-01']['verification_key'],
+                'dns_token' => $acmeValues['dns-01']['token'],
+            ];
+        }
+
+        if (count($data) === 0) {
+            $this->log->warning("You have no Domains that match the following.");
+        }
+
+        // Sort by Domain Name.
+        usort($data, function( $a, $b ) {
+            if ($a['domain'] === $b['domain']) {
+                return 0;
+            }
+            return $a['domain'] > $b['domain'] ? 1 : -1;
+        });
+        
+        return new RowsOfFields($data);
+    }
+}
